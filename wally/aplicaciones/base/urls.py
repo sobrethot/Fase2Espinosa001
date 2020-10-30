e1a92d2b8a0699bd312d0406bb54d97f9dea5804
unknown <walterespinosaponce@gmail.com>
2020-10-30 00:47:13 -0300

    Creando Formularios

--- wally/aplicaciones/base/urls.py
+++ wally/aplicaciones/base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import Inicio,Listado
+from .views import Inicio,Listado,FormularioContacto,DetallePost
 
 urlpatterns = [
     path('',Inicio.as_view(), name = 'index'),
     path('videojuegos/',Listado.as_view(),{'nombre_categoria':'Videojuegos'}, name = 'videojuegos'),
     path('generales/',Listado.as_view(),{'nombre_categoria':'General'}, name = 'generales'),
+    path('formulario_contacto/', FormularioContacto.as_view(), name = 'formulario_contacto'),
+    path('<slug:slug>/',DetallePost.as_view(), name = 'detalle_post'),
 ]
--- wally/aplicaciones/base/views.py
+++ wally/aplicaciones/base/views.py
@@ -1,8 +1,9 @@
 import random
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render,redirect
+from django.views.generic import ListView,View,DetailView
 from .models import Post,Categoria,RedesSociales,Web
 from .utils import *
+from .forms import ContactoForm
 
 
 
@@ -67,7 +68,55 @@ class Listado(ListView):
         contexto = generarCategoria(request,nombre_categoria)
         return render(request,'categoria.html',contexto)
 
+class FormularioContacto(View):
+    def get(self,request,*args,**kwargs):
+        form = ContactoForm()
+        contexto = {
+            'sociales':obtenerRedes(),
+            'web':obtenerWeb(),
+            'form':form,
 
+        }
+        return render(request,'contacto.html',contexto)
+    
+    def post(self,request,*args,**kwargs):
+        form = ContactoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('base:index')
+        else:
+            contexto = {
+                'form':form,
+            }
+            return render(request,'contacto.html',contexto)
+    
+class DetallePost(DetailView):
+    def get(self,request,slug,*args,**kwargs):
+        try:
+            post = Post.objects.get(slug = slug)
+        except:
+            post = None
+        posts = list(Post.objects.filter(
+                estado = True,
+                publicado = True
+                ).values_list('id',flat = True))
+        posts.remove(post.id)
+        post1 = random.choice(posts)
+        posts.remove(post1)
+        post2 = random.choice(posts)
+        posts.remove(post2)
+        post3 = random.choice(posts)
+        posts.remove(post3)
+
+        contexto = {
+            'post':post,
+            'sociales':obtenerRedes(),
+            'web':obtenerWeb(),
+            'post1':consulta(post1),
+            'post2':consulta(post2),
+            'post3':consulta(post3),
+        }
+        return render(request,'post.html',contexto)
 
 
 
